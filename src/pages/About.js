f267f8e6d07e7e723cf5972744cbc296e8ecbb7c
ManonVillanueva <g.manon14@gmail.com>
2024-10-09 09:53:30 +0200

    map countries

---
+++ src/pages/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla tenetur
+        consequuntur, sint veniam iusto hic amet delectus voluptate perferendis
+        commodi ipsam reprehenderit voluptatum eveniet id officia. Ipsam neque
+        voluptas aperiam tenetur impedit, soluta debitis, atque, ex sequi
+        consequatur dolorum alias enim quis vel nulla optio. Tenetur placeat
+        aliquid facilis eos architecto optio earum eaque minus, quae sit
+        doloribus excepturi dolorum dicta explicabo quod suscipit voluptas,
+        assumenda eveniet deserunt. Similique placeat est esse veniam aspernatur
+        corporis ipsam corrupti tempore repudiandae obcaecati error fugiat
+        libero maiores numquam a, voluptatibus minus nam voluptate consequatur
+        velit dolore reiciendis, nobis sequi. Laboriosam obcaecati provident
+        vitae.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa amet
+        ipsum, ullam asperiores itaque mollitia commodi totam ea nisi aspernatur
+        ipsa praesentium? Debitis aspernatur sed, temporibus aut nostrum atque
+        autem, maiores magnam accusamus incidunt architecto itaque officiis eos
+        numquam repellendus facere fuga vitae nesciunt molestias pariatur. Quo
+        deserunt error fuga cum voluptatum, nam ducimus maiores pariatur dolor.
+        Ea magnam qui voluptatum vero exercitationem nemo eligendi, quam
+        accusantium consequuntur est earum ipsum excepturi voluptas officiis
+        accusamus in iusto unde numquam tempora? Deserunt aliquid, totam est
+        repellendus libero quo ea, necessitatibus sint ut neque, maxime vero?
+        Ipsam labore minus, incidunt saepe temporibus dignissimos enim nulla
+        rerum laborum corporis ratione ut id cumque voluptas ducimus placeat
+        porro autem harum mollitia eos perspiciatis. Laboriosam cum, adipisci
+        voluptates cupiditate saepe eveniet aliquid vero, odit eius nemo neque
+        ipsa nobis? Obcaecati aliquam debitis quos ullam dolorum animi tenetur
+        facilis? Natus eius quod, ipsa, vel eaque odit consequuntur veniam aut
+        molestias ratione porro quis cumque consequatur, optio odio fuga rerum
+        provident? Ipsum suscipit maxime excepturi distinctio natus earum fugiat
+        beatae incidunt. Odit dolore facilis quibusdam, nostrum perferendis
+        suscipit sapiente esse eaque explicabo id modi earum officiis. Placeat
+        facilis maxime aperiam quos, reprehenderit aliquid ipsa voluptatem
+        dolorem adipisci?
+      </p>
+    </div>
+  );
+};
+
+export default About;
